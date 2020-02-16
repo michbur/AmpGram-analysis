3ddcc0728e9cd960dbbba448832f41d2f8f55fb8
ksidorczuk <sidorczuk.katarzyna17@gmail.com>
2020-02-16 14:30:28 +0100

    added docs and prints

--- functions/get_mers.R
+++ functions/get_mers.R
@@ -1,4 +1,9 @@
-create_hv <- function(seq) 
+#' Create mer dataframe
+#' 
+#' Creates dataframe of 10-mers
+#' @param seq matrix of sequences created by \code{\link{list2matrix}}
+#' @return dataframe of mers with their source peptide and mer ID
+create_mer_df <- function(seq) 
   do.call(rbind, lapply(1L:nrow(seq), function(i) {
     seq2ngrams(seq[i, ][!is.na(seq[i, ])], 10, a()[-1]) %>% 
       decode_ngrams() %>% 
@@ -7,10 +12,19 @@ create_hv <- function(seq)
       do.call(rbind, .) %>% 
       data.frame(stringsAsFactors = FALSE) %>% 
       mutate(source_peptide = rownames(seq)[i],
-             mer_id = paste0(source_peptide, "h", 1L:nrow(.)))
+             mer_id = paste0(source_peptide, "m", 1L:nrow(.)))
   }))
 
-
+#' Get dataframe of mers
+#' 
+#' This function creates a dataframe of 10-mers with columns indicating 
+#' their source peptide, mer ID, group of source peptide, fold and target.
+#' @param pos positive dataset
+#' @param pos_id IDs of the positive dataset
+#' @param neg negative dataset
+#' @param neg_id IDs of the negative dataset
+#' @return a dataframe of 10-mers with their source peptides, mer IDs, group,
+#' target and fold
 get_mers <- function(pos, pos_id, neg, neg_id) {
   seq_groups <- lapply(names(pos_id), function(i)
     c(pos[pos_id[[i]][["traintest"]]], neg[neg_id[[i]][["traintest"]]])) %>% 
@@ -28,10 +42,13 @@ get_mers <- function(pos, pos_id, neg, neg_id) {
     
     ith_group %>% 
       list2matrix() %>% 
-      create_hv %>% 
+      create_mer_df %>% 
       mutate(group = ith_group_id) %>% 
       inner_join(fold_df, by = c("source_peptide" = "source_peptide"))
   }) %>% 
     do.call(rbind, .) %>% 
-    mutate(target = grepl("AMP", source_peptide, fixed = TRUE))
+    mutate(target = grepl("AMP", source_peptide, fixed = TRUE)) %T>% {
+    print(paste0("Number of AMP mers: ", nrow(filter(target == TRUE))))
+    print(paste0("Number of non-AMP mers: ", nrow(filter(target == FALSE))))
+    }
 }  
--- functions/writing_benchmarks.R
+++ functions/writing_benchmarks.R
@@ -1,3 +1,12 @@
+#' Writes benchmark dataset into the file
+#' 
+#' Filters sequences from the positive and negative datasets for benchmark 
+#' and writes them into the file in fasta format.
+#' @param pos positive dataset 
+#' @param pos_id IDs of the positive dataset
+#' @param neg negative dataset
+#' @param neg_id IDs of the negative dataset
+#' @return a file containing sequences from benchmark dataset in fasta format
 write_benchmark <- function(pos, pos_id, neg, neg_id) {
   seq_list <- c(pos[unlist(lapply(pos_id, function(ith_len_group) ith_len_group[["benchmark"]]))],
     neg[unlist(lapply(neg_id, function(ith_len_group) ith_len_group[["benchmark"]]))]) 
