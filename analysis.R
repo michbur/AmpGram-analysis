1f7e14c0f01906e9edae59efc9aa9ec7b4522dd0
ksidorczuk <sidorczuk.katarzyna17@gmail.com>
2020-02-20 10:35:25 +0100

    changed '<-' to '=' in drake plan

--- analysis.R
+++ analysis.R
@@ -47,8 +47,8 @@ analysis_AmpGram <- drake_plan(raw_data = read_raw_data(),
                                ngrams3_2 = count_ampgrams(mer_df, 
                                                           ns = c(3, 3),
                                                           ds = list(c(1, 0), c(1, 1))),
-                               binary_ngrams <- cbind(ngrams12, ngrams3_1, ngrams3_2),
-                               cv_raw <- do_cv(mer_df, binary_ngrams))
+                               binary_ngrams = cbind(ngrams12, ngrams3_1, ngrams3_2),
+                               cv_raw = do_cv(mer_df, binary_ngrams))
 
 make(analysis_AmpGram, seed = 990)
 
