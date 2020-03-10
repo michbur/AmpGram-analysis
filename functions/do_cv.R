c8c7e10a78a09bc16fa1f20efa113dda4fb4831e
Michał Burdukiewicz <michalburdukiewicz@gmail.com>
2020-03-10 13:43:34 +0100

    added test do_cv

--- functions/do_cv.R
+++ functions/do_cv.R
@@ -39,3 +39,38 @@ do_cv <- function(mer_df, binary_ngrams)
     }) %>% bind_rows()
   ) %>% bind_rows()
 
+
+do_cv_test <- function(mer_df, binary_ngrams)
+  lapply(mer_df[["group"]], function(ith_group) 
+    pblapply(mer_df[["fold"]], function(ith_fold) {
+      train_dat <- filter(mer_df, group == ith_group, fold != ith_fold)
+      test_dat <- filter(mer_df, group == ith_group, fold == ith_fold)
+      
+      test_bis <- test_features(train_dat[["target"]],
+                                binary_ngrams[mer_df[["group"]] == ith_group & 
+                                                mer_df[["fold"]] != ith_fold, ])
+      
+      imp_bigrams <- cut(test_bis, breaks = c(0, 0.05, 1))[[1]]
+      
+      ranger_train_data <- data.frame(as.matrix(binary_ngrams[mer_df[["group"]] == ith_group & 
+                                                                mer_df[["fold"]] != ith_fold, imp_bigrams]),
+                                      tar = as.factor(train_dat[["target"]]))
+      model_cv <- ranger(tar ~ ., ranger_train_data, write.forest = TRUE, probability = TRUE, num.trees = 2000)
+      
+      preds <- mutate(test_dat,
+                      pred = predict(model_cv, 
+                                     data.frame(as.matrix(binary_ngrams[mer_df[["group"]] == ith_group & mer_df[["fold"]] == ith_fold, imp_bigrams])))[["predictions"]][, "TRUE"])
+      
+      # single mer predictions
+      #HMeasure(preds[["target"]], preds[["pred"]])[["metrics"]]
+      
+      peptide_preds <- group_by(preds, source_peptide, target) %>% 
+        summarise(peptide_pred = max(pred))
+      
+      rbind(HMeasure(as.numeric(peptide_preds[["target"]]), 
+                     peptide_preds[["peptide_pred"]])[["metrics"]],
+            fold = ith_fold,
+            group = ith_group)
+    }) %>% bind_rows()
+  ) %>% bind_rows()
+
