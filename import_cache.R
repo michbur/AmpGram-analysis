bba1ec4d912d05a23f32837c310888cd2f480622
ksidorczuk <sidorczuk.katarzyna17@gmail.com>
2020-04-24 22:30:00 +0200

    updated import

--- import_cache.R
+++ import_cache.R
@@ -5,10 +5,8 @@ import_cache <- function() {
   if (Sys.info()[["nodename"]] == "kasia-MACH-WX9") {
     data_path <- "/home/kasia/Dropbox/AmpGram-analysis/"
   }
-  
-  file.copy(from = paste0(data_path, 
-                          "drake-cache/", 
-                          list.files("drake-cache/")), 
-            to = ".drake", recursive = TRUE, overwrite = TRUE)
-  
+
+  file.copy(from = paste0(data_path,"drake-cache/.drake/",
+                          list.files(paste0(data_path, "drake-cache/.drake/"))),
+            to = ".drake/", recursive = TRUE, overwrite = TRUE)
 }
